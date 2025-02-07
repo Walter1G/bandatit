66d08124c39c47a05af44dc83ef2589958ffb115
Walter <2760walter@gmail.com>
2025-02-08 01:08:25 +0300

    Sketch HomeLayout Element

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,28 @@
+import logo from "../../Assets/logo.png";
 const Footer = () => {
-  return <div>Footer</div>;
+    return <footer>
+        <div className="footer-content">
+            <div className="footer-left">
+                <img src={logo} className="logo-icon" alt="logo" />
+                <p>Bandatit</p>
+
+            </div>
+            <div className="footer-center">
+                <ul>
+                    <li>Central City Nairobi</li>
+                    <li>P.o Box 1123</li>
+                    <li>City Center</li>
+                </ul>
+            </div>
+            <div className="footer-right">
+                <ul>
+                    <li>theBand@it.co.ke</li>
+                    <li>+254708393794</li>
+                    <li>223849400404</li>
+                    </ul>
+                </div>
+      </div>
+  </footer>;
 };
 
 export default Footer;
